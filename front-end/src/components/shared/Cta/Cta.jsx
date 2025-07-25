04c358bd5a90ab0f0b4dea45e3aa6f0d28a99a13
FaisalHasanEmon <faisalhasan5833@gmail.com>
2025-07-25 06:59:43 +0600

    updated

--- front-end/src/components/shared/Cta/Cta.jsx
+++ front-end/src/components/shared/Cta/Cta.jsx
@@ -4,7 +4,7 @@ import SecondaryButton from "../Buttons/SecondaryButton";
 
 const Cta = () => {
   return (
-    <div className="bg-gradient-to-r from-[#f0f5ef] via-base-10 to-[#f0f5ef] py-16 md:px-5 text-center">
+    <div className="bg-gradient-to-r from-[#f0f5ef] via-base-10 to-[#f0f5ef] py-16 px-5 text-center">
       <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-6">
         Ready to take control of your health?
       </h2>

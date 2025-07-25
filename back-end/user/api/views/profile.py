3ecddc81872878f6eb99d48a7e1a851dbd6f5807
shagorrobidas <roysagor88@gmail.com>
2025-07-26 00:00:01 +0600

    create profile view

--- back-end/user/api/views/profile.py
+++ back-end/user/api/views/profile.py
@@ -1,10 +1,44 @@
-from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
+from rest_framework import serializers
 from user.models import Profile
-from user.api.serializers.profile import ProfileSerializer
+from user.api.serializers import ProfileSerializer
 
 
-class ProfileListView(ListCreateAPIView):
+class ProfileCreateView(ListCreateAPIView):
     serializer_class = ProfileSerializer
-    model = Profile
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        if Profile.objects.filter(user=self.request.user).exists():
+            raise serializers.ValidationError("You already have a profile.")
+        serializer.save(user=self.request.user)
     
+
+class ProfileDetailView(RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
+    
+
+class ProfileUpdateView(UpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
+    
+
+class ProfileDeleteView(DestroyAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)

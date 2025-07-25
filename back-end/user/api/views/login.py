58dd1bf2eeb8f63f85986a87a6a3a098fd02c4a2
shagorrobidas <roysagor88@gmail.com>
2025-07-26 01:09:45 +0600

    send mail un veryfyed user

--- back-end/user/api/views/login.py
+++ back-end/user/api/views/login.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from user.api.serializers.login import LoginSerializer
+from user.utils import send_verification_email
 
 
 class LoginView(APIView):
@@ -15,8 +16,9 @@ class LoginView(APIView):
         user = serializer.validated_data['user']
 
         if not user.email_verified:
+            send_verification_email(user, request)
             raise AuthenticationFailed("Please verify your email before logging in.")
-
+        
         refresh = RefreshToken.for_user(user)
 
         return Response({

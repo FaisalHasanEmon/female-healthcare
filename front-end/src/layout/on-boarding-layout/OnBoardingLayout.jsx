9a1ff3244caff8205ab7aafcb4b154c09b6d321d
Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-07-27 03:59:25 +0600

    set the blob

--- front-end/src/layout/on-boarding-layout/OnBoardingLayout.jsx
+++ front-end/src/layout/on-boarding-layout/OnBoardingLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import OnBoardingNavbar from "../../components/shared/OnBoardingNavbar/OnBoardingNavbar";
 import { Outlet } from "react-router-dom";
+// import "./onboarding.css"
 
 const OnBoardingLayout = () => {
   return (
-    <div className="relative min-h-screen">
+    // <div className="relative min-h-screen blob1 blob2 blob3 color-onboading backdrop-blur-md">
+    <div className="relative min-h-screen overflow-x-hidden" >
       <OnBoardingNavbar className="mx-auto"></OnBoardingNavbar>
       <div className="flex justify-center items-center h-screen">
         <Outlet/>
@@ -33,6 +35,12 @@ const OnBoardingLayout = () => {
           className="h-[170px] md:h-[570px]"
         />
       </figure>
+      {/* blob1 */}
+      <div className="bg-[#D4E6C14D] h-[456px] w-[610px] absolute rounded-[68%_32%_65%_35%_/_64%_31%_69%_36%] -left-[194px] top-[136px] blur-[200px]"></div>
+      {/* blob2 */}
+      <div className="bg-[#D4E6C14D] h-[912px] w-[868px] absolute rounded-[50%_50%_32%_68%_/_52%_43%_57%_48%] -left-[356px] top-[427px] blur-[200px] rotate-[132deg] "></div>
+      {/* blob3 */}
+      <div className="bg-[#D4E6C14D] h-[913px] w-[868px] absolute rounded-[62%_38%_15%_85%_/_52%_49%_51%_48%]  -right-[200px] -top-[299px] blur-[200px]  rotate-[353deg] "></div>
     </div>
   );
 };

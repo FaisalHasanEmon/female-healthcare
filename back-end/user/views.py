2aaf917806baeeff8cf5908882953f6adb093e82
shagorrobidas <roysagor88@gmail.com>
2025-07-26 06:12:58 +0600

    Password reset confurm view

--- back-end/user/views.py
+++ back-end/user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class PasswordResetConfirmPageView(TemplateView):
+    template_name = "email/confirm_password.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['uidb64'] = kwargs['uidb64']
+        context['token'] = kwargs['token']
+        return context

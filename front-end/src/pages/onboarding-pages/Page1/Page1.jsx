bbacf9f7741c48f070aac1e3879b05967fb95391
Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-07-27 06:38:46 +0600

    bug fix from page1 form one error stay

--- front-end/src/pages/onboarding-pages/Page1/Page1.jsx
+++ front-end/src/pages/onboarding-pages/Page1/Page1.jsx
@@ -1,83 +1,120 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
-
-
-
-
-
-
 // first form
 const Page1 = () => {
-
-  const [cycle, setCycle] = useState("");
+  const [period, setPeriod] = useState(false);
+  const [cycle, setCycle] = useState({});
   const navigate = useNavigate();
 
-  // get cycle 
-  useEffect(()=>{
+  // get cycle
+  useEffect(() => {
     const getCycle = localStorage.getItem("cycle");
-    if(getCycle?.length > 0){
-      setCycle(getCycle)
+    if (getCycle && Object.keys(getCycle).length > 0) {
+      setCycle(getCycle);
     }
-  },[])
+  }, []);
 
-
-  // set cycle 
-  // useEffect(()=>{
-  //    if(cycle?.length > 0){
-  //     localStorage.setItem("cycle", cycle);
-  //    }
-  // },[cycle])
-
+  
   //set cycle in localstorage
-  const handleValues=()=>{
-       if(cycle?.length > 0){
-      localStorage.setItem("cycle", cycle);
-     }
-    navigate("/onboarding/page2")
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const healthStatus = event.target.healthStatus.value;
+    console.log(healthStatus);
+    
+    if (healthStatus === "periods"){
+      const date = event.target.date.value;
+      const cycleDay = event.target.cycleDay.value;
+      const periodDuration = event.target.periodDuration.value;
+      const cycleData = { date, cycleDay, periodDuration };
+      setCycle(cycleData);
+    } else {
+      const cycleData = { healthStatus: "yes" };
+      setCycle(cycleData);
+    }
+    if (Object.keys(cycle).length > 0) {
+      localStorage.setItem("cycle", JSON.stringify(cycle));
+      navigate("/onboarding/page2");
+    }
+  };
 
-
-
-  // rander ui 
+  // rander ui
   return (
-    <div className="text-center flex flex-col gap-10 w-3/4">
+    <div className="text-center flex flex-col gap-10 w-9/12 md:w-6/12">
       <h1 className="text-2xl md:text-3xl font-bold font-playfair-display">
         Tell us about your cycle
       </h1>
-      <form
-        onChange={(event) => setCycle(event.target.value)}
-        className="flex flex-col gap-3 md:gap-5"
-      >
-         {/* maped for options */}
-        {options.map((data) => (
-          <label
-            key={data.id}
-            className="border border-brandPrimary rounded flex justify-between px-2 py-2 md:p-3 items-center"
-          >
-            <span className="font-montserrat ">{data?.title}</span>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3 md:gap-5">
+        <label className="border border-brandPrimary rounded flex justify-between px-2 py-2 md:p-3 items-center cursor-pointer">
+          <span className="font-montserrat ">Do you have periods?</span>
+          <input
+            onChange={()=>setPeriod(true)}
+            type="radio"
+            className=""
+            name="healthStatus"
+            value="periods"
+          />
+        </label>
+        <label className="border border-brandPrimary rounded flex justify-between px-2 py-2 md:p-3 items-center cursor-pointer">
+          <span className="font-montserrat ">Are you in menopause?</span>
+          <input
+          onChange={()=>setPeriod(false)}
+            type="radio"
+            className=""
+            name="healthStatus"
+            value="menopause"
+          />
+        </label>
+        <label className="border border-brandPrimary rounded flex justify-between px-2 py-2 md:p-3 items-center cursor-pointer">
+          <span className="font-montserrat ">Are you on hormone therapy?</span>
+          <input
+           onChange={()=>setPeriod(false)}
+            type="radio"
+            className=""
+            name="healthStatus"
+            value="hormoneTherapy"
+          />
+        </label>
+        <div
+          className={`flex flex-col gap-3 md:gap-5 ${
+            period ? "block" : "hidden"
+          }`}
+        >
+          <label className="border border-brandPrimary rounded flex justify-between px-2 py-2 md:p-3 items-center">
+            <span className="font-montserrat ">Last period start date</span>
             <input
-              checked={data.value === cycle}
-              type="radio"
-              className=""
-              name="healthStatus"
-              value={data?.value}
-              onChange={() => {}}
+              type="date"
+              className="input input-sm outline-1 cursor-pointer px-1 outline-brandPrimary rounded-sm"
             />
           </label>
-        ))}
-      </form>
-
-      <div>
+          <label className="border border-brandPrimary rounded flex justify-between px-2 py-2 md:p-3 items-center">
+            <span className="font-montserrat ">Cycle length (Days)</span>
+            <input
+              type="number"
+              className="outline-1 cursor-pointer outline-brandPrimary px-1 rounded-sm"
+              defaultValue={28}
+              name="cycleDay"
+            />
+          </label>
+          <label className="border border-brandPrimary rounded flex justify-between px-2 py-2 md:p-3 items-center">
+            <span className="font-montserrat ">Period duration (Days)</span>
+            <input
+              type="number"
+              className="outline-1 cursor-pointer px-1 outline-brandPrimary rounded-sm"
+              defaultValue={7}
+              name="periodDuration"
+            />
+          </label>
+        </div>
+        <div>
         <button
-          onClick={handleValues}
+          type="submit"
           className=" btn bg-brandPrimary text-center px-5 md:px-40 hover:bg-[#7f9e90]"
         >
           Continue
         </button>
       </div>
+      </form>
     </div>
   );
 };
@@ -86,20 +123,20 @@ export default Page1;
 
 //--------------------- from options data for page1-----------------------------
 
-const options = [
-  {
-    id: 1,
-    title: "Do you have periods?",
-    value: "periods",
-  },
-  {
-    id: 2,
-    title: "Are you in menopause?",
-    value: "menopause",
-  },
-  {
-    id: 3,
-    title: "Are you on hormone therapy?",
-    value: "hormoneTherapy",
-  },
-];
+// const options = [
+//   {
+//     id: 1,
+//     title: "Do you have periods?",
+//     value: "periods",
+//   },
+//   {
+//     id: 2,
+//     title: "Are you in menopause?",
+//     value: "menopause",
+//   },
+//   {
+//     id: 3,
+//     title: "Are you on hormone therapy?",
+//     value: "hormoneTherapy",
+//   },
+// ];

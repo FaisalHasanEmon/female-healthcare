1533b6f6b14c552e23bb2dc7d09d54f45cb643db
shagorrobidas <roysagor88@gmail.com>
2025-08-02 05:42:56 +0600

    profile and email update

--- back-end/user/api/serializers/profile_update.py
+++ back-end/user/api/serializers/profile_update.py
@@ -1,12 +1,20 @@
+from django.utils import timezone
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from user.models import Profile, User
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.urls import reverse
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
 
 
 class ProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['name', 'age', 'date_of_birth']
+        fields = ['name', 'age', 'address']
 
 
 class UserProfileUpdateSerializer(serializers.ModelSerializer):
@@ -14,8 +22,8 @@ class UserProfileUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['email', 'email_verified', 'profile']
-    
+        fields = ['email', 'new_email', 'email_verified', 'profile']
+
     def validate_email(self, value):
         user = self.context['request'].user
         if User.objects.exclude(pk=user.pk).filter(email=value).exists():
@@ -24,21 +32,57 @@ class UserProfileUpdateSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         profile_data = validated_data.pop('profile', {})
-        
-        # Update User email
-        instance.email = validated_data.get('email', instance.email)
-        instance.email_verified = validated_data.get(
-            'email_verified', instance.email_verified
-        )
-        instance.save()
+        new_email = validated_data.pop('new_email', None)
 
-        # Update Profile fields
-        profile = instance.profile
-        profile.name = profile_data.get('name', profile.name)
-        profile.age = profile_data.get('age', profile.age)
-        profile.date_of_birth = profile_data.get(
-            'date_of_birth', profile.date_of_birth
-        )
-        profile.save()
+        # Update profile
+        if profile_data:
+            profile_serializer = self.fields['profile']
+            profile_serializer.update(instance.profile, profile_data)
+
+        # Start email change process
+        if new_email and new_email != instance.email:
+            self._initiate_email_change(instance, new_email)
 
         return instance
+
+    def _initiate_email_change(self, user, new_email):
+        user.new_email = new_email
+        user.email_change_token = default_token_generator.make_token(user)
+        user.email_change_requested_at = timezone.now()
+        user.save()
+        self._send_old_email_verification(user)
+
+    def _send_old_email_verification(self, user):
+        print(f"Sending verification to old email: {user.email}")
+
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        token = user.email_change_token
+        verification_url = self.context['request'].build_absolute_uri(
+            reverse('verify_email_change', kwargs={'uidb64': uid, 'token': token})
+        )
+
+        subject = "Confirm Email Change"
+        context = {
+            'current_email': user.email,
+            'new_email': user.new_email,
+            'verification_url': verification_url,
+        }
+
+        text_content = render_to_string(
+            'email/email_change_verification.txt',
+            context
+        )
+        html_content = render_to_string(
+            'email/email_change_verification.html',
+            context
+        )
+
+        email = EmailMultiAlternatives(
+            subject,
+            text_content,
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email]
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send()
+

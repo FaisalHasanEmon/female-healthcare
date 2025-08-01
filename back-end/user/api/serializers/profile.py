9617c7ddc8a29e836dd140635855f91ac9f4004c
shagorrobidas <roysagor88@gmail.com>
2025-08-02 05:40:51 +0600

    bug fix

--- back-end/user/api/serializers/profile.py
+++ back-end/user/api/serializers/profile.py
@@ -1,23 +1,9 @@
 from rest_framework import serializers
-from user.models import User
+from user.models import Profile, User
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = (
-            'id',
-            'name',
-            'age',
-            'date_of_birth',
-            'profile_picture',
-            'adderess')
-        read_only_fields = ['user']
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-
-
-
+        model = Profile
+        fields = '__all__'
+        read_only_fields = ('user',)

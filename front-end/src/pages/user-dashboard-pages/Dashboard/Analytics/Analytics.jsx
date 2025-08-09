f4cdb6f25b6fa6affbdde37f1024db7fb4f36762
FaisalHasanEmon <faisalhasan5833@gmail.com>
2025-08-09 06:30:52 +0600

    analytics page updated

--- front-end/src/pages/user-dashboard-pages/Dashboard/Analytics/Analytics.jsx
+++ front-end/src/pages/user-dashboard-pages/Dashboard/Analytics/Analytics.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Analytics = () => {
-  return <div className="bg-red-500 ">This is analytics page.</div>;
+  return (
+    <section>
+      <div className="grid md:grid-cols-2 lg:grid-cols-12 border">
+        <div className="h-5 w-5/12 border border-red-200"></div>
+        <div className="h-5 w-5/12 border border-red-200"></div>
+        <div className="h-5 w-2/12 border border-red-200"></div>
+        <div className="h-5 w-5/12 border border-red-200"></div>
+        <div className="h-5 w-5/12 border border-red-200"></div>
+        <div className="h-5 w-2/12 border border-red-200"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Analytics;
